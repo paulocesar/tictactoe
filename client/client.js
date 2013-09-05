75cf9b790ac1abced4a8e40d32a5f3a04c2f9381
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2013-09-04 21:27:12 -0300

    Melhorias na jogabilidade

--- client/client.js
+++ client/client.js
@@ -1,18 +1,38 @@
-game = new Meteor.Stream('game');
+Game = new Meteor.Stream('game');
 
-Template.userInfo.events({
-  "submit .form": function(event) {
-    var user = $(event.target).find('input').val();
-    console.log("Username: %s", user);
-    Session.set('username', user);
-    game.emit('start', user);
-    event.preventDefault();
+Game.on('start', function(play, turn, weapon) {
+  if(play) {
+    if(!Session.get('play')) {
+      console.log("Weapon: %s | Turn: %s | Play: %s", weapon, turn, play);
+      Session.set('weapon', weapon);
+      Session.set('play', play);
+      Session.set('turn', turn);
+    }
+  } else {
+    if(!Session.get('play')) {
+      alert("Sorry, you can't enter on this game.");
+      Session.set('username', null);
+    }
   }
 });
 
-game.on('weapon', function(weapon) {
-  console.log("Weapon: %s", weapon);
-  Session.set('weapon', weapon);
+Game.on('update', function(weapon, row, col) {
+  if(Session.get('play')) {
+    var board = $('.gameboard');
+    var target = board.find('.row[data-row="'+ row +'"]');
+    target = target.find('.col[data-col="'+ col +'"]');
+    target.html('<i class="'+ weapon +'"></i>');
+  }
+});
+
+Template.userInfo.events({
+  "submit .form": function(event) {
+    var user = $(event.target).find('.input').val();
+    console.log("Username: %s", user);
+    Session.set('username', user);
+    Game.emit('initialize', user);
+    event.preventDefault();
+  }
 });
 
 Template.userInfo.username = function() {
@@ -23,10 +43,9 @@ Template.gameBoard.events({
   "click .col": function(event) {
     if(Session.get('username')) {
       var weapon = Session.get('weapon');
-      var x = $(event.target);
-      var y = x.closest('.row');
-      x.html('<i class="'+ weapon +'"></i>');
-      console.log("[Row: %s | Col: %s]", y.data('row'), x.data('col'));
+      var col = $(event.target);
+      var row = col.closest('.row');
+      Game.emit('shoot', weapon, row.data('row'), col.data('col'));
     } else {
       alert("First you need to enter your name!");
       $('.input').focus();
--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
-var weapons = ['icon-x', 'icon-o'];
-game = new Meteor.Stream('game');
+var WEAPONS = ['icon-x', 'icon-o'];
+var BOARD = [[],[],[]];
 
-game.on('start', function(username) {
-	if(weapons.length) {
+Game = new Meteor.Stream('game');
+
+Game.permissions.write(function() { return true; });
+Game.permissions.read(function() { return true; });
+
+Game.on('initialize', function(username) {
+	var limit = WEAPONS.length; 
+	if(limit) {
+		var turn = (limit % 2 == 0) ? true : false;
+		var weapon = WEAPONS.pop();
 		console.log("%s has started the game!", username);
-		game.emit('weapon', weapons.pop());
+		Game.emit('start', true, turn, weapon);
 	} else {
-		console.log("%s you can't start the game!", username);
+		console.log("%s you can't start on this game!", username);
+		Game.emit('start', false);
 	}
 });
 
-game.permissions.write(function(eventName) {
-  return true;
-});
-
-game.permissions.read(function(eventName) {
-  return true;
+Game.on('shoot', function(weapon, row, col) {
+	console.log("Weapon: %s | Row: %s | Col: %s", weapon, row, col);
+	Game.emit('update', weapon, row, col);
 });
